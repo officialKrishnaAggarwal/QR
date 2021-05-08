e0c53cd23fd7ab5e1d6cf144aeb2783819bda6b9
officialKrishnaAggarwal <78261130+officialKrishnaAggarwal@users.noreply.github.com>
2021-05-08 15:42:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import {Text} from 'react-native'
+import QR from './Screens/QR'
+export default class App extends React.Component{
+  render(){
+    return(
+      <QR/>
+    )
+  }
+}
